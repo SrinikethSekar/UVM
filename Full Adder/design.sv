5168a9883343a1479ce7ecad87f2b628c61914a6
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-06-02 23:01:28 +0530

    Create design.sv

---
+++ Full Adder/design.sv
@@ -0,0 +1,16 @@
+module full_adder(
+        input  a    ,
+        input  b    ,
+        input  c    ,
+
+        output sum  ,
+        output carry     
+);
+  
+  assign sum   = a^b^c;
+  
+  assign carry = (a&b)|(b&c)|(a&c);
+  
+endmodule
+
+//https://edaplayground.com/x/H2_f
